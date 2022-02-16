3774adbf7580ad7d62035aae85f61aebeeefdf46
xuyaxiong <xyxlindy@163.com>
2022-02-16 13:52:18 +0800

    first commit

---
+++ src/main/java/com/example/testjenkins01/TestJenkins01Application.java
@@ -0,0 +1,13 @@
+package com.example.testjenkins01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkins01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkins01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testjenkins01/TestJenkins01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjenkins01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkins01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
